45cfc3791aea2e8a1a7bce06c461ef182d2a53b6
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2022-04-19 14:51:32 +0530

    Update wlrs.py

--- wlrs.py
+++ wlrs.py
@@ -1,6 +1,13 @@
 import os
+import pdb
+from pdb import set_trace
 
 
 a = [1, 2, 3, 4]
 if (n := len(a)) > 3:
     print(f"List is too long ({n} elements, expected <= 3)")
+
+    
+pdb.set_trace()
+set_trace()
+breakpoint()
